df2a66bd9c298fbf812b397cc5873f18ce13574f
SHRIYAASK <shriyaaskraj.gmail.com>
2024-12-01 21:25:52 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        Calculator f= new Calculator();
+        f.setSize(240,400);
+        f.setVisible(true);
+        f.setTitle("Calculator");
+    }
+
+}
